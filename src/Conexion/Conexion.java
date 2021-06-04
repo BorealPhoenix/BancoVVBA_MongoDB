bc1cafd1e2f27b50ec311e146a076fd74ab54346
manana <manana@Aula1.8-C14>
2021-06-04 14:10:07 +0200

    Conexion a MongoDB actualizada y operativa 	nuevo archivo:  out/production/subirNotaProgramacion/Conexion/Conexion.class 	nuevo archivo:  out/production/subirNotaProgramacion/modelo/DAO/CuentaDAO.class 	nuevo archivo:  out/production/subirNotaProgramacion/modelo/DAO/CuentaDAOSQL.class 	nuevo archivo:  out/production/subirNotaProgramacion/modelo/DTO/Cuenta.class 	nuevo archivo:  out/production/subirNotaProgramacion/test/HolaMundo.class 	modificado:     src/Conexion/Conexion.java

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -1,34 +1,44 @@
 package Conexion;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
+import com.mongodb.client.*;
+import org.bson.Document;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.MongoClient;
 
-
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Iterator;
 
 
 public class Conexion {
     private static Conexion conexion;
-    private Connection connection;
+    private Connection dataBase;
+//Usar literable:
 
-    public Conexion() {
-        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-        MongoDatabase baseDeDatos =  mongoClient.getDatabase("db1");
-        MongoCollection collection = baseDeDatos.getCollection("accounts");
-        System.out.println(collection);
-    }
-
-    public Connection getConexion() {
-        return connection;
-    }
-
-        public static void main(String[] args) {
-
-            Conexion conexion = new Conexion();
-            System.out.println(conexion);
-            System.out.println(conexion.getConexion());
-
+    private Conexion() {
+        MongoClient mongo = new MongoClient("localhost", 27017);
+        MongoDatabase database = mongo.getDatabase("db1");
+        MongoCollection<Document> collection = database.getCollection("accounts");
+        FindIterable<Document> iterDoc = collection.find();
+        Iterator it = iterDoc.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
         }
     }
 
+    public Connection getConexion() {
+        return dataBase;
+    }
+
+    public static Conexion getInstance() throws SQLException, IOException {
+        if (conexion == null) {
+            conexion = new Conexion();
+        }
+        return conexion;
+    }
+
+    public static void main(String args[]) {
+        Conexion conexion = new Conexion();
+        System.out.println(conexion);
+    }
+}
